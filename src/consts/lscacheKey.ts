db8549192ce29a2cefa1aa8f9e014023ba8b101a
hotarupoyo <29631883+hotarupoyo@users.noreply.github.com>
2024-01-05 16:20:31 +0900

    他とキャッシュを共有できるように、キーをURLに変更した

--- src/consts/lscacheKey.ts
+++ src/consts/lscacheKey.ts
@@ -1,6 +1,5 @@
 import { contestId, userId } from "./atcoder";
 
-export const lscacheKeyPrefix = "atcoder-navigation-dropdown";
-export const lscacheKeyTop = `${lscacheKeyPrefix}-top`;
-export const lscacheKeyMyScores = `${lscacheKeyPrefix}-my-scores-${contestId}`;
-export const lscacheKeyMySubmissions = `${lscacheKeyPrefix}-my-submissions-${userId}-${contestId}`;
+export const lscacheKeyTop = `https://atcoder.jp/contests/${contestId}`;
+export const lscacheKeyMyScores = `https://atcoder.jp/contests/${contestId}/score`;
+export const lscacheKeyMySubmissions = `https://atcoder.jp/contests/${contestId}/submissions?f.User=${userId}`;
